f963b8f4b074767d697ea7c76fd8a19b551102e2
dannytimothy <d.timothy.wallace@gmail.com>
2020-08-19 22:32:29 -0400

    Yes

--- MT_Manager/manage.py
+++ MT_Manager/manage.py
@@ -3,6 +3,7 @@ import sys
 from flask.cli import FlaskGroup
 from project import create_app, db   
 from project.api.models import User 
+import pytest
 
 
 app = create_app()  
@@ -14,7 +15,10 @@ def recreate_db():
     db.drop_all()
     db.create_all()
     db.session.commit()
-
+@cli.command('seed_db')
+def seed_db():
+    db.session.add(User(username='Danny',email="danny@nitotech.com"))
+    db.session.commit()
 
 if __name__ == '__main__':
     cli()
